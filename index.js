337ac34b8e3d7b9fbea9eae724bacd61a9454cd0
Guillermo Martinez <dumblemoe@gmail.com>
2020-03-10 20:51:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cards = (["Lisa", "Kaitlin", "Jan"], "birthday");
 
 function writeCards(cards) {
   for (let i = 0; i < cards.length; i++) {
-    console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
   }
   return cards;
 }
