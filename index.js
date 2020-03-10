0fcdffd06083b39817f0ff5512a632a2b752a4e9
Guillermo Martinez <dumblemoe@gmail.com>
2020-03-10 20:43:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // Code your solutions in this file
-writeCards([])
+const cards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!");
+  }
+  return cards;
+}
