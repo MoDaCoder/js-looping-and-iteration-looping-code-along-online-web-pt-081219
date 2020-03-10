54743907aa8cfcd7b88a3186b3c35c1a56f125bf
Guillermo Martinez <dumblemoe@gmail.com>
2020-03-10 20:49:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function writeCards(cards) {
   for (let i = 0; i < cards.length; i++) {
     console.log(`Thank you, ${cards[i]}, for the wonderful birthday gift!`);
   }
-  
   return cards;
-  
 }
 
-
+writeCards(cards);
